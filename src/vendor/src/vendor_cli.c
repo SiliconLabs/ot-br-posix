f8e7ca10fb3bb89ea99b0268465456777cdae040
Mason Tran <Mason.Tran@silabs.com>
2020-11-11 14:05:35 -0500

    Add an example command to vendor_cli.c

--- src/vendor/src/vendor_cli.c
+++ src/vendor/src/vendor_cli.c
@@ -1,12 +1,26 @@
 #include OPENTHREAD_POSIX_VENDOR_CLI_HEADER
 
+#include <openthread/cli.h>
+
+static void HandleMyCommand(void)
+{
+    // Intentionally empty
+}
+
+otCliCommand vendorCommands[] = {
+    {"myCommand", HandleMyCommand},
+};
+
+
 /**
- * Override this function to add vendor commands
+ * Set user commands to vendor commands
  *
  * @param[in]  aContext       @p The context passed to the handler.
  *
  */
-void vendorCliInit(void *aContext)
+void vendorCliInit(void)
 {
-    // Intentionally empty
+    const uint8_t numCommands = sizeof(vendorCommands)) / (sizeof(otCliCommand);
+
+    otCliSetUserCommands(&radioUrlCommand, numCommands, &config.mPlatformConfig);
 }
