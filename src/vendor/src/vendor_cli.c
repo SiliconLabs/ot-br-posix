6d5a86bcd9980a4846549a76b0eb9c3b8059475b
Mason Tran <Mason.Tran@silabs.com>
2020-11-11 13:41:43 -0500

    Add optional ot-vendor-cli cmake library

---
+++ src/vendor/src/vendor_cli.c
@@ -0,0 +1,12 @@
+#include OPENTHREAD_POSIX_VENDOR_CLI_HEADER
+
+/**
+ * Override this function to add vendor commands
+ *
+ * @param[in]  aContext       @p The context passed to the handler.
+ *
+ */
+void vendorCliInit(void *aContext)
+{
+    // Intentionally empty
+}
