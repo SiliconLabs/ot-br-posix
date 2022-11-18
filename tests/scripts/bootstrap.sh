8c4a37f8bbc37624639712a2deef45f3c1cffd9b
Bob MacDonald <Robert.MacDonald@silabs.com>
2022-11-17 19:12:16 -0500

    OPENTHREAD-2777 - address mDNSResponder-878.30.4.tar.gz 404 Not Found
    Applying the fix that was made in https://github.com/openthread/ot-br-posix/pull/1592

--- tests/scripts/bootstrap.sh
+++ tests/scripts/bootstrap.sh
@@ -134,8 +134,9 @@ case "$(uname)" in
 
         if [ "${OTBR_MDNS-}" == 'mDNSResponder' ]; then
             SOURCE_NAME=mDNSResponder-878.30.4
-            wget https://opensource.apple.com/tarballs/mDNSResponder/$SOURCE_NAME.tar.gz \
-                && tar xvf $SOURCE_NAME.tar.gz \
+            wget https://github.com/apple-oss-distributions/mDNSResponder/archive/refs/tags/$SOURCE_NAME.tar.gz \
+                && mkdir -p $SOURCE_NAME \
+                && tar xvf $SOURCE_NAME.tar.gz -C $SOURCE_NAME --strip-components=1 \
                 && cd $SOURCE_NAME/mDNSPosix \
                 && make os=linux && sudo make install os=linux
         fi
